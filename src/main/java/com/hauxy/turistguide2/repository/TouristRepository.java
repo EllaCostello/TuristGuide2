5154e4fc5eb0170be36896bcf08000f5fe366f3a
EllaCostello <ella.costello@yahoo.com>
2025-09-01 15:17:08 +0200

    Kopi klar fra turistguide 1, ingen nye additions endnu, blot en tro kopi til et nyt repo

---
+++ src/main/java/com/hauxy/turistguide2/repository/TouristRepository.java
@@ -0,0 +1,51 @@
+package com.hauxy.turistguide2.repository;
+
+import com.hauxy.turistguide2.model.TouristAttraction;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class TouristRepository {
+    List<TouristAttraction> attractions = new ArrayList<>();
+
+    public TouristRepository() {
+        populateTouristAttraction();
+    }
+
+    public void populateTouristAttraction() {
+        attractions.add(new TouristAttraction("Tivoli", "Verdens ældste tivoli"));
+        attractions.add(new TouristAttraction("Den Lille Havfrue", "Verdens ældste havfrue"));
+        attractions.add(new TouristAttraction("Operaen", "Verdens ældste opera"));
+
+
+    }
+
+    public List<TouristAttraction> getTouristAttractions() {
+        return attractions;
+    }
+
+    public void addTouristAttraction(TouristAttraction touristAttraction) {
+        attractions.add(touristAttraction);
+    }
+
+    public void updateTouristAttraction(String name, String updateDescription) {
+        for (TouristAttraction t : getTouristAttractions()) {
+            if (name.equals(t.getName())) {
+                t.setDescription(updateDescription);
+            }
+        }
+    }
+
+
+    public void removeTouristAttraction(String name) {
+        for (TouristAttraction t : attractions) {
+            if (name.equals(t.getName())) {
+                attractions.remove(t);
+            }
+        }
+    }
+
+
+}
---
+++ src/main/java/com/hauxy/turistguide2/service/TouristService.java
@@ -0,0 +1,43 @@
+package com.hauxy.turistguide2.service;
+
+import com.hauxy.turistguide2.model.TouristAttraction;
+import com.hauxy.turistguide2.repository.TouristRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TouristService {
+    TouristRepository touristRepository;
+
+    public TouristService(TouristRepository touristRepository) {
+        this.touristRepository = touristRepository;
+    }
+    public void populateTouristAttraction() {
+        touristRepository.populateTouristAttraction();
+    }
+
+    public List<TouristAttraction> getTouristAttractions() {
+        return touristRepository.getTouristAttractions();
+    }
+
+    public TouristAttraction getAttraction(String name) {
+        for (TouristAttraction t : getTouristAttractions()) {
+            if (name.equals(t.getName())) {
+                return t;
+            }
+        }
+        return null;
+    }
+
+    public void updateTouristAttraction(String name, String updateDescription) {
+        touristRepository.updateTouristAttraction(name, updateDescription);
+    }
+
+    public void addTouristAttraction(TouristAttraction touristAttraction) {
+        touristRepository.addTouristAttraction(touristAttraction);
+    }
+    public void removeTouristAttraction(String name) {
+        touristRepository.removeTouristAttraction(name);
+    }
+}
